f6f1b42903dd85e2e69c05433d5411ad9affa010
Azaduddin2001 <mdazaduddin2001@gmail.com>
2025-03-13 00:26:45 +0600

    verson BB

--- Inheritance2.java
+++ Inheritance2.java
@@ -2,6 +2,7 @@ public class Inheritance2{
     public static void main(String[] args) {
         System.out.println("My name is md azad uddin");
         System.out.println("Iam student,i study in bsc ");
+        System.out.println("my univercity name of Rajshahi univercity");
     }
     
 }
